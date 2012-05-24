25eb7bba85e0a6c10ffdddd9bab3d8c68d9fd0a8
shen.yuchen <shen.yuchen@canon-system.com.cn>
2012-05-24 11:07:49 +0800

    Make rainbow faces auto-generation customizable

--- rainbow-delimiters.el
+++ rainbow-delimiters.el
@@ -285,10 +285,109 @@ Non-nil stop cyclically highlight nested delimiters."
 ;;; Faces 10+:
 ;; NOTE: Currently unused. Additional faces for depths 9+ can be added on request.
 
-(defconst rainbow-delimiters-max-face-count 9
+(defvar rainbow-delimiters-max-face-count 9
   "Number of faces defined for highlighting delimiter levels.
 
 Determines depth at which to cycle through faces again.")
+
+;;; Rainbow Faces
+;; NOTE: Auto-generated faces
+
+(defun hsv->rgb (hue saturation value)
+  " hue: 0~360, saturation/value: 0.0~1.0, red/green/blue:00~FF "
+  (let (red green blue int-hue fract pp qq tt ww)
+    (if (= saturation 0.0)
+	(setq red value
+	      green value
+	      blue value)
+      (setq hue (/ hue 60.0)
+	    int-hue (floor hue)
+	    fract (- hue int-hue)
+	    pp (* value (- 1 saturation))
+	    qq (* value (- 1 (* saturation fract)))
+	    ww (* value (- 1 (* saturation (- 1 fract)))))
+      (case int-hue
+	((0 6) (setq red value
+		     green ww
+		     blue pp))
+	(1 (setq red qq
+		 green value
+		 blue pp))
+	(2 (setq red pp
+		 green value
+		 blue ww))
+	(3 (setq red pp
+		 green qq
+		 blue value))
+	(4 (setq red ww
+		 green pp
+		 blue value))
+	(otherwise (setq red value
+			 green pp
+			 blue qq))))
+    (mapcar #'(lambda (color)
+		(floor (* 255 color)))
+	    (list red green blue))))
+
+(defun rainbow-foreground (band primaries &optional saturation value)
+  " generate #XXXXXX format color string"
+  (destructuring-bind (red green blue)
+      (hsv->rgb (/ (* 360 band) primaries) (or saturation 1.0) (or value 1.0))
+    (format "#%02X%02X%02X" red green blue)))
+
+(defmacro rainbow-delimiters-rainbow-faces (primaries &rest facebody)
+  " generate bunch of defface or custom-set-faces for specified spectrum primaries"
+  (let ((primaries (eval primaries)))
+    (prog1
+	(destructuring-bind (specs &rest restbody)
+	    facebody
+	  `(progn
+	     ,@(loop for depth from 1 to primaries
+		     collect
+		     (let* ((face (intern (concat (symbol-name 'rainbow-delimiters-depth-) (format "%d" depth) (symbol-name '-face))))
+			    (newspec (loop for spec in specs
+					   collect
+					   (let* ((face-attr (copy-tree (second spec)))
+						  (foreground-args (getf face-attr :foreground))
+						  (foreground-brightness (getf foreground-args :brightness))
+						  (foreground-saturation (getf foreground-args :saturation))
+						  (height-range (getf face-attr :height))
+						  (height-from (getf height-range :from))
+						  (height-to (getf height-range :to))
+						  (height-delta (and height-to height-from (/ (* (- height-to height-from) depth) primaries)))
+						  (height (and height-delta (+ height-from height-delta))))
+					     ;;destructuring op safety here?
+					     (setf (getf face-attr :foreground) (rainbow-foreground depth primaries
+												    (and (floatp foreground-saturation) foreground-saturation)
+												    (and (floatp foreground-brightness) foreground-brightness)))
+					     (when height
+					       (setf (getf face-attr :height) height))
+					     (list (first spec) face-attr))))
+			    (defbody `(,face
+				       ',newspec
+				       ,@restbody))
+			    (cusbody `(,face
+				       ,newspec
+				       ,@restbody)))
+		       (if (facep face)
+			   `(custom-set-faces ',cusbody)
+			 `(defface ,@defbody))))))
+      (setq rainbow-delimiters-max-face-count primaries))))
+
+(defcustom rainbow-delimiters-generate-rainbow-faces-p nil ;(
+  "Generate rainbow-like faces automatically"
+  :tag "Generate rainbow faces?"
+  :type 'integer
+  :group 'rainbow-delimiters-toggle-delimiter-highlighting
+  :set (lambda (symbol value)
+	 (set-default symbol value)
+	 (when (natnump value)
+	   (rainbow-delimiters-rainbow-faces value
+	     				     ((((background light)) (:foreground (:brightness 0.5 :saturation 1.0) :height (:from 1.5 :to 1.0)))
+	     				      (((background dark)) (:foreground (:brightness 1.0 :saturation 0.8) :height (:from 1.5 :to 1.0))))
+	     				     "Generated rainbow faces for nested delimiters."
+	     				     :group 'rainbow-delimiters-faces)))
+  )
 
 ;;; Face utility functions
 
